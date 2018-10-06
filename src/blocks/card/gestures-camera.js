cef57e69470b57158138e8dbc9914d3651d60a55
Ivan Dashkevich <idashkevich@lement.pro>
2018-10-06 13:45:46 +0300

    Zoom fix on large touch screens

--- src/blocks/card/gestures-camera.js
+++ src/blocks/card/gestures-camera.js
@@ -11,15 +11,19 @@ const turnGestureInterfaceOn = () => {
 
 const setGestures = () => {
     let currentGesture = [];
+    let initialFingerDistance = undefined;
+
+    const cameraStyle = window.getComputedStyle(camera);
+    const initialBackgroundSize = parseInt(cameraStyle.getPropertyValue('background-size').slice(0, -2));
 
     const constants = {
         zoomSpeedModifier: 0.1,
-        minimumBackgroundSize: 500,
-        maximumBackgroundSize: 1000,
+        minimumBackgroundSize: initialBackgroundSize,
+        maximumBackgroundSize: initialBackgroundSize + 500,
     };
 
     const cameraState = {
-        currentBackgroundSize: 500,
+        currentBackgroundSize: initialBackgroundSize,
         currentPosition: 0,
     };
 
@@ -32,10 +36,17 @@ const setGestures = () => {
             currentBackgroundSize: cameraState.currentBackgroundSize,
             currentPosition: cameraState.currentPosition,
         });
+
+        if (!event.isPrimary) {
+            initialFingerDistance = Math.sqrt(
+                Math.pow(currentGesture[0].startX - currentGesture[1].startX, 2) + 
+                Math.pow(currentGesture[0].startY - currentGesture[1].startY, 2));
+        }
     });
 
     camera.addEventListener('pointermove', (event) => {
-        switch (currentGesture.length) {
+        const fingersCount = currentGesture.length;
+        switch (fingersCount) {
             case 0:
                 return;
             case 1:
@@ -57,28 +68,21 @@ const setGestures = () => {
 
     const zoom = (event) => {
         const {pointerId, x, y} = event;
-        const movedfinger = pointerId === currentGesture[0].pointerId 
+        const fixedFinger = pointerId !== currentGesture[0].pointerId 
             ? currentGesture[0] 
             : currentGesture[1];
-        const fixedfinger = movedfinger.pointerId === currentGesture[1].pointerId 
-            ? currentGesture[0] 
-            : currentGesture[1];
-
-        const initialDistance = Math.sqrt(
-            Math.pow(movedfinger.startX - fixedfinger.startX, 2) + 
-            Math.pow(movedfinger.startY - fixedfinger.startY, 2));
 
         const newDistance = Math.sqrt(
-            Math.pow(x - fixedfinger.startX, 2) + 
-            Math.pow(y - fixedfinger.startY, 2));
+            Math.pow(x - fixedFinger.startX, 2) + 
+            Math.pow(y - fixedFinger.startY, 2));
 
-        const zoom = (newDistance - initialDistance) * constants.zoomSpeedModifier;
+        const zoom = (newDistance - initialFingerDistance) * constants.zoomSpeedModifier;
         const newBackgroundSize = cameraState.currentBackgroundSize + zoom;
 
         if (newBackgroundSize < constants.minimumBackgroundSize || newBackgroundSize > constants.maximumBackgroundSize) return;
 
-        cameraState.currentBackgroundSize = newBackgroundSize;
         camera.style.backgroundSize = `${cameraState.currentBackgroundSize}px`;
+        cameraState.currentBackgroundSize = newBackgroundSize;
 
         const zoomPercentValue = Math.round((newBackgroundSize - constants.minimumBackgroundSize) 
             / (constants.maximumBackgroundSize - constants.minimumBackgroundSize) * 100);
