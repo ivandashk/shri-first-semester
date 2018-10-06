855bf92667c166aa6106ad34f9d5c8cae95b2f76
Ivan Dashkevich <idashkevich@lement.pro>
2018-10-06 13:16:27 +0300

    Touch zoom

--- src/blocks/card/gestures-camera.js
+++ src/blocks/card/gestures-camera.js
@@ -10,33 +10,85 @@ const turnGestureInterfaceOn = () => {
 }
 
 const setGestures = () => {
-    let currentGesture = null;
+    let currentGesture = [];
+
+    const constants = {
+        zoomSpeedModifier: 0.1,
+        minimumBackgroundSize: 500,
+        maximumBackgroundSize: 1000,
+    };
 
     const cameraState = {
+        currentBackgroundSize: 500,
         currentPosition: 0,
     };
 
     camera.addEventListener('pointerdown', (event) => {
         camera.setPointerCapture(event.pointerId);
-        currentGesture = {
+        currentGesture.push({
+            pointerId: event.pointerId,
             startX: event.x,
+            startY: event.y,
+            currentBackgroundSize: cameraState.currentBackgroundSize,
             currentPosition: cameraState.currentPosition,
-        };
+        });
     });
 
     camera.addEventListener('pointermove', (event) => {
-        if (!currentGesture) return;
+        switch (currentGesture.length) {
+            case 0:
+                return;
+            case 1:
+                panX(event);
+                break;
+            case 2:
+                zoom(event);
+                break;
+        }
+    });
 
-        const {startX, currentPosition} = currentGesture;
+    const panX = (event) => {
+        const {startX, currentPosition}  = currentGesture[0];
         const {x} = event;
         const dx = x - startX;
         camera.style.backgroundPositionX = `${currentPosition + dx}px`;
         cameraState.currentPosition = currentPosition + dx;
-    });
+    }
+
+    const zoom = (event) => {
+        const {pointerId, x, y} = event;
+        const movedfinger = pointerId === currentGesture[0].pointerId 
+            ? currentGesture[0] 
+            : currentGesture[1];
+        const fixedfinger = movedfinger.pointerId === currentGesture[1].pointerId 
+            ? currentGesture[0] 
+            : currentGesture[1];
+
+        const initialDistance = Math.sqrt(
+            Math.pow(movedfinger.startX - fixedfinger.startX, 2) + 
+            Math.pow(movedfinger.startY - fixedfinger.startY, 2));
+
+        const newDistance = Math.sqrt(
+            Math.pow(x - fixedfinger.startX, 2) + 
+            Math.pow(y - fixedfinger.startY, 2));
+
+        const zoom = (newDistance - initialDistance) * constants.zoomSpeedModifier;
+        const newBackgroundSize = cameraState.currentBackgroundSize + zoom;
+
+        if (newBackgroundSize < constants.minimumBackgroundSize || newBackgroundSize > constants.maximumBackgroundSize) return;
+
+        cameraState.currentBackgroundSize = newBackgroundSize;
+        camera.style.backgroundSize = `${cameraState.currentBackgroundSize}px`;
+
+        const zoomPercentValue = Math.round((newBackgroundSize - constants.minimumBackgroundSize) 
+            / (constants.maximumBackgroundSize - constants.minimumBackgroundSize) * 100);
+
+        cameraInterface.firstElementChild.innerHTML = `Приближение: ${zoomPercentValue}%`;
+    }
 
     const moveToStartPosition = () => {
-        if (!currentGesture) return;
-        currentGesture = null;
+        if (currentGesture.length === 0) return;
+        currentGesture = [];
     }
 
     camera.addEventListener('pointerup', moveToStartPosition);
