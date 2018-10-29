ed018ce3f943f4f87a8ead425c7f7f3d0965978f
Ivan Dashkevich <idashkevich@lement.pro>
2018-10-29 14:40:22 +0300

    Добавлены функции-инициализаторы nav.ts и sidebar.ts

--- src/blocks/nav/nav.ts
+++ src/blocks/nav/nav.ts
@@ -1,19 +1,23 @@
-const navItems = document.getElementsByClassName("nav__item");
-const navs = document.getElementsByClassName("nav");
+const initNavs = () => {
+    const navItems = document.getElementsByClassName("nav__item");
+    const navs = document.getElementsByClassName("nav");
 
-for (const nav of navs) {
-    nav.children[0].classList.toggle("nav__item_active");
-}
+    for (const nav of navs) {
+        nav.children[0].classList.toggle("nav__item_active");
+    }
 
-for (const navItem of navItems) {
-    navItem.addEventListener("click", (e) => {
-        const target = cast(e.target, HTMLElement);
-        const targetParent = target.parentNode as ParentNode;
+    for (const navItem of navItems) {
+        navItem.addEventListener("click", (e) => {
+            const target = cast(e.target, HTMLElement);
+            const targetParent = target.parentNode as ParentNode;
 
-        const otherOptions = targetParent.children;
-        for (const otherOption of otherOptions) {
-            otherOption.classList.remove("nav__item_active");
-        }
-        target.classList.toggle("nav__item_active");
-    });
-}
+            const otherOptions = targetParent.children;
+            for (const otherOption of otherOptions) {
+                otherOption.classList.remove("nav__item_active");
+            }
+            target.classList.toggle("nav__item_active");
+        });
+    }
+};
+
+initNavs();
--- src/blocks/sidebar/sidebar.ts
+++ src/blocks/sidebar/sidebar.ts
@@ -1,8 +1,12 @@
-const toggleSideBar = cast(document.getElementById("toggle-sidebar"), HTMLElement);
-const sidebar = cast(document.getElementById("sidebar"), HTMLElement);
+const initSidebar = () => {
+    const toggleSideBar = cast(document.getElementById("toggle-sidebar"), HTMLElement);
+    const sidebar = cast(document.getElementById("sidebar"), HTMLElement);
 
-if (!!toggleSideBar && !!sidebar) {
-    toggleSideBar.addEventListener("click", () => {
-        sidebar.classList.toggle("sidebar_opened");
-    });
-}
+    if (!!toggleSideBar && !!sidebar) {
+        toggleSideBar.addEventListener("click", () => {
+            sidebar.classList.toggle("sidebar_opened");
+        });
+    }
+};
+
+initSidebar();
